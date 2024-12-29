3e0271d38c443b9a5212705cf24cf8770ac13309
GNRbaskaran <gnani.baskaran30@gmail.com>
2024-12-29 17:23:32 +0530

    commits

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,7 @@ require('dotenv').config()
 app.use(express.json());
 app.use(cors({
     origin: ['http://localhost:5173', 'https://book-app-frontend-tau.vercel.app'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }))
 
